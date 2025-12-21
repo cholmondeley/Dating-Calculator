77e7654a21c8bd344f6ff10a8bb9c91433235d90
Vetruvian <vetruvian@Vetruvians-MacBook-Air.local>
2025-12-21 16:03:46 -0500

    fajlkdfsljf

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: false,
+  content: ['./index.html', './{index,App}.tsx', './components/**/*.{ts,tsx}', './**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
